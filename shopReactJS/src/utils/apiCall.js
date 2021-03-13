4fc34494aaaa13507cf4af64d91b4ded475abe77
tuki <huybeos2707@gmail.com>
2021-03-13 08:39:43 +0700

    add

--- shopReactJS/src/utils/apiCall.js
+++ shopReactJS/src/utils/apiCall.js
@@ -4,7 +4,8 @@ import * as Config from './../constants/Config';
 export default function callApi(endpoint, method = 'GET', body){
     return axios({
         method: method,
-        url: `${Config.API_URL}/${endpoint}`,
+        // url: `${Config.API_URL}/${endpoint}`,
+        url: `http://171.244.8.156:1000/api/${endpoint}`,
         data: body
     }).catch(err => {
         console.log(err)
